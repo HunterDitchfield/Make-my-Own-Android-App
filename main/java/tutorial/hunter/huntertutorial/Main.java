ba351607c0fcad617b71fb2eff55c996093a1e86
HunterDitchfield <37520905+HunterDitchfield@users.noreply.github.com>
2019-01-12 23:37:36 -0500

    Add files via upload
    Follow the YouTube video as a guide line to copy and paste this code into your Android Studio project.

---
+++ main/java/tutorial/hunter/huntertutorial/Main.java
@@ -0,0 +1,16 @@
+package tutorial.hunter.huntertutorial;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Hunter on 1/7/2019.
+ */
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
